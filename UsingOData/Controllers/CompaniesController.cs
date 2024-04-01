c77c46f5138ee6e5b5db6374ded56d5984bc6c94
Mushtaq Ahmed <mushtaq.a@gmail.com>
2024-04-01 21:10:57 +0530

    init

---
+++ UsingOData/Controllers/CompaniesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Formatter;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Results;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+namespace UsingOData.Controllers
+{
+    public class CompaniesController(ICompanyRepo repo) : ODataController
+    {
+        [EnableQuery(PageSize = 3)]
+        public IQueryable<Company> Get()
+        {
+            return repo.GetAll();
+        }
+
+        [EnableQuery]
+        public SingleResult<Company> Get([FromODataUri] int key)
+        {
+            return SingleResult.Create(repo.GetById(key));
+        }
+
+        public IActionResult Post([FromBody] Company company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            repo.Create(company);
+
+            return Created("companies", company);
+        }
+
+        public IActionResult Put([FromODataUri] int key, [FromBody] Company company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (key != company.ID)
+            {
+                return BadRequest();
+            }
+
+            repo.Update(company);
+
+            return NoContent();
+        }
+
+        public IActionResult Delete([FromODataUri] int key)
+        {
+            var company = repo.GetById(key);
+            if (company is null)
+            {
+                return BadRequest();
+            }
+
+            repo.Delete(company.First());
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ UsingODataUnitTests/UsingODataIntegrationTests.cs
@@ -0,0 +1,158 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+
+namespace UsingODataIntegrationTests
+{
+    public class UsingODataIntegrationTests
+    {
+        private HttpClient _httpClient;
+        private WebApplicationFactory<Program> _application;
+
+        public UsingODataIntegrationTests()
+        {
+            _application = new WebApplicationFactory<Program>();
+            _httpClient = _application.CreateClient();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            _application.Dispose();
+            _httpClient.Dispose();
+        }
+        
+        
+        
+
+        [Test]
+        public async Task WhenQueringForAllCompanies_ThenThreeCompaniesAreReturned()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7004/odata/Companies");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            dynamic json = JsonConvert.DeserializeObject(responseString);
+            Assert.Equals(json.value.Count, 3);
+
+            dynamic id = json.value[0].ID.ToString();
+            Assert.Equals(id, "1");
+
+            id = json.value[1].ID.ToString();
+            Assert.Equals(id, "2");
+
+            id = json.value[2].ID.ToString();
+            Assert.Equals(id, "3");
+        }
+
+        [Test]
+        public async Task WhenQueringForACompany_ThenTheCorrectCompanyIsReturned()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7004/odata/Companies(2)");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            dynamic json = JsonConvert.DeserializeObject(responseString);
+            Assert.Equals(json.ID.ToString(), "2");
+        }
+
+        [Test]
+        public async Task WhenSkippingOneCompany_ThenTheNextThreeCompaniesAreReturned()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7004/odata/Companies?$skip=1");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            dynamic json = JsonConvert.DeserializeObject(responseString);
+            Assert.Equals(json.value.Count, 3);
+
+            dynamic id = json.value[0].ID.ToString();
+            Assert.Equals(id, "2");
+
+            id = json.value[1].ID.ToString();
+            Assert.Equals(id, "3");
+
+            id = json.value[2].ID.ToString();
+            Assert.Equals(id, "4");
+        }
+
+        [Test]
+        public async Task WhenSkippingOneAndAskingForTwo_ThenTheNextTwoCompaniesAreReturned()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7004/odata/Companies?$top=2&$skip=1");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            dynamic json = JsonConvert.DeserializeObject(responseString);
+            Assert.Equals(json.value.Count, 2);
+
+            dynamic id = json.value[0].ID.ToString();
+            Assert.Equals(id, "2");
+
+            id = json.value[1].ID.ToString();
+            Assert.Equals(id, "3");
+        }
+
+        [Test]
+        public async Task WhenFilteringById_ThenCorrectCompanyIsReturned()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7004/odata/Companies?$filter=ID eq 1");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            dynamic json = JsonConvert.DeserializeObject(responseString);
+            Assert.Equals(json.value.Count, 1);
+
+            dynamic id = json.value[0].ID.ToString();
+            Assert.Equals(id, "1");
+        }
+
+        [Test]
+        public async Task WhenFilteringByCompanySize_ThenCorrectCompaniesAreReturned()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7004/odata/Companies?$filter=Size gt 50");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            dynamic json = JsonConvert.DeserializeObject(responseString);
+            Assert.Equals(json.value.Count, 2);
+
+            dynamic id = json.value[0].ID.ToString();
+            Assert.Equals(id, "2");
+
+            id = json.value[1].ID.ToString();
+            Assert.Equals(id, "4");
+        }
+
+        [Test]
+        public async Task WhenOrderingBySize_ThenOrderIsReturned()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7004/odata/Companies?$orderby=Size Desc");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            dynamic json = JsonConvert.DeserializeObject(responseString);
+            Assert.Equals(json.value.Count, 3);
+
+            dynamic id = json.value[0].ID.ToString();
+            Assert.Equals(id, "4");
+
+            id = json.value[1].ID.ToString();
+            Assert.Equals(id, "2");
+
+            id = json.value[2].ID.ToString();
+            Assert.Equals(id, "1");
+        }
+    }
+}
